3667acc51a9c149848499fc700e3d5533eee9599
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-30 16:08:42 -0300

    Update GeraPalpites.tsx

--- src/components/GeraPalpites.tsx
+++ src/components/GeraPalpites.tsx
@@ -20,7 +20,9 @@ const GeraPalpites = () => {
     setLoading(true);
     try {
       // 1. URL corrigida apontando para a rota de estatísticos
-      const response = await fetch('https://palpiteiro-backend.vercel.app/');
+      const response = await fetch(
+  "https://palpiteiro-backend.vercel.app/palpites/estatistico"
+);
       const data = await response.json();
 
       // 2. Acessa data.palpites (o objeto retornado pelo FastAPI tem essa chave)
