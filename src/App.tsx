bc7e39cea91ea11c7971d3933b18b3507138875e
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-01-02 11:45:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Palpites from "./pages/Palpites";
 import Estatisticas from "./pages/Estatisticas";
@@ -11,10 +12,12 @@ import Historico from "./pages/Historico";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
+import { ProtectedRoute } from "@/components/ProtectedRoute";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 2, // Tenta 2 vezes antes de mostrar erro
+      retry: 2,
       staleTime: 1000 * 60 * 5, // 5 minutos
     },
   },
@@ -27,12 +30,45 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* Páginas públicas */}
           <Route path="/" element={<Home />} />
-          <Route path="/palpites" element={<Palpites />} />
-          <Route path="/estatisticas" element={<Estatisticas />} />
-          <Route path="/resultados" element={<Resultados />} />
-          <Route path="/historico" element={<Historico />} />
           <Route path="/auth" element={<Auth />} />
+
+          {/* Páginas privadas */}
+          <Route
+            path="/palpites"
+            element={
+              <ProtectedRoute>
+                <Palpites />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/estatisticas"
+            element={
+              <ProtectedRoute>
+                <Estatisticas />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/resultados"
+            element={
+              <ProtectedRoute>
+                <Resultados />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/historico"
+            element={
+              <ProtectedRoute>
+                <Historico />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Página 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
