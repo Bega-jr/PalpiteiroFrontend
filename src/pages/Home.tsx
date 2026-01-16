5ea3452b256977c9466c75dcb0b1d8d9d31973b5
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-01-16 16:06:54 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,9 @@ function Home() {
     staleTime: 1000 * 60 * 60,
   });
 
-  const c = Array.isArray(rawData) ? rawData[0] : rawData;
+  const c = rawData && typeof rawData === "object" && "concurso" in rawData
+  ? rawData
+  : null;
 
   const forceRefresh = () => {
     queryClient.invalidateQueries({ queryKey: ["home"] });
