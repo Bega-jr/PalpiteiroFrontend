65d63d9683b0f15333e64137e56de566911bc712
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-05-22 12:14:26 +0000

    remove node_modules do repositorio

--- src/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-
-export default function AuthPage() {
-  return (
-    <main className="min-h-screen bg-slate-50 flex items-center justify-center p-4">
-      <AuthForm />
-    </main>
-  );
-}
--- src/app/resultados/page.tsx
+++
@@ -1,9 +0,0 @@
-import ResultadosOficiais from '@/components/ResultadosOficiais';
-
-export default function ResultadosPage() {
-  return (
-    <main className="min-h-screen bg-slate-50 py-10">
-      <ResultadosOficiais />
-    </main>
-  );
-}
